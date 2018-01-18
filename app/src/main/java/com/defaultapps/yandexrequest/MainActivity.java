abf5bc554071b6ef166c91886b34097bf75d251c
Kirill Petrov <petrov.kirill.job@gmail.com>
2018-01-18 12:59:59 +0300

    return response in Activity

--- app/src/main/java/com/defaultapps/yandexrequest/MainActivity.java
+++ app/src/main/java/com/defaultapps/yandexrequest/MainActivity.java
@@ -9,7 +9,14 @@ import android.widget.EditText;
 
 import com.defaultapps.yandexrequest.network.NetworkService;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,15 +28,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ns = new NetworkService(getApplicationContext());
+        ns = new NetworkService();
 
         editText = findViewById(R.id.editText);
         button = findViewById(R.id.buttonRequest);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ns.sendRequest(editText.getText().toString());
+                ns.sendRequest(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        Log.e("onFailure", "Error - ", e);
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        Log.d("onResponse", "callSuccessful");
+                        saveHtml(response.body().string());
+                    }
+                }, editText.getText().toString());
             }
         });
     }
+
+    public void saveHtml(String str) {
+        File file = new File(getApplicationContext().getFilesDir().getPath() + "/request.html");
+
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(file));
+            out.write(str);
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            Log.e("SaveFileError", e.toString());
+        }
+    }
+
 }
